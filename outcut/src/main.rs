81b016e84bd2a84598aa9a64bcaedcebaee8163a
Ben Berry <bsberry@protonmail.com>
2021-12-24 10:57:16 -0500

    Experimental support for other OSes

--- outcut/src/main.rs
+++ outcut/src/main.rs
@@ -1,10 +1,9 @@
-use std::io;
-
 use dialoguer::Select;
-use std::process::{Command, Stdio};
-use std::borrow::BorrowMut;
-use std::io::{Write, Stdin};
 use isatty::stdout_isatty;
+use std::borrow::BorrowMut;
+use std::io;
+use std::process::{Command,Stdio};
+use std::io::{Write, Stdin};
 
 fn main() -> io::Result<()>{
     let mut lines: Vec<String> = Vec::new();
@@ -19,7 +18,7 @@ fn main() -> io::Result<()>{
     select.with_prompt("Choose line");
         // .items(foo)
     select.default(0);
-    select.clear(false);
+    // select.clear(false);
 
     for line in &lines {
         select.item(line.trim());
@@ -30,15 +29,38 @@ fn main() -> io::Result<()>{
     let selected_line = lines.get(selection).unwrap().trim();
 
     if stdout_isatty() {
-        let mut pbcopy = Command::new("pbcopy");
-        pbcopy.stdin(Stdio::piped());
-        let mut stdin = pbcopy.spawn()?.stdin.unwrap();
-        let stdin_ref = stdin.borrow_mut();
-        stdin_ref.write(selected_line.as_bytes());
-        stdin_ref.flush();
+        copy_to_clipboard(selected_line);
     } else {
         print!("\"{}\"", selected_line); // Print to stdout in case you piped to something else
     }
 
     Ok(())
+}
+
+fn copy_to_clipboard(line: &str) {
+    let commands = ["xclip", "pbcopy"];
+    for command in commands {
+        let mut which = Command::new("which");
+        which
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .arg(command);
+        let success = which.status().unwrap().success();
+        if (success) {
+            println!("Command found: {}", command);
+            do_copy_to_clipboard(command, line);
+            return;
+        }
+    }
+
+    println!("No commands of list [{}] found installed in system to copy to clipboard.", commands.join(", "));
+}
+
+fn do_copy_to_clipboard(command: &str, line: &str) {
+    let mut cmd = Command::new(command);
+    cmd.stdin(Stdio::piped());
+    let mut stdin = cmd.spawn().expect("error running command").stdin.unwrap();
+    let stdin_ref = stdin.borrow_mut();
+    stdin_ref.write(line.as_bytes()).expect("error writing lines to be copied");
+    stdin_ref.flush().expect("error flushing to command");
 }
